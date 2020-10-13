370ca929f4aac0177e9a5126d05979a5190648ea
gowni <47999529+gowni@users.noreply.github.com>
2020-10-12 19:59:05 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,40 +1,36 @@
-////////////////////////////////////////////////////////////////////////////////
-// Main File: queue.h
-// This File: queue.h
-// This File Description: This is the reader that also sets up the queue struct
-// Author:           William Hofkamp, Pranet Gowni
-// Email:            hofkamp@wisc.edu, gowni@wisc.edu
-// CS Login:         hofkamp, pranet
-////////////////////////////////////////////////////////////////////////////////
 #include <semaphore.h>
 
-#ifndef QUEUE_SERVICE
-#define QUEUE_SERVICE
 
-typedef struct {
-    int size;
-    int numElem;
-    int head; 
+//Queue definition
+#ifndef STRUCT_STAT_STATM_CMDLINE
+#define STRUCT_STAT_STATM_CMDLINE
+typedef struct Queue
+{
+    int head;
     int tail;
-    int enqueueCount;
+    int capacity;
+    int numElements;
+    int enqueueCount;
     int dequeueCount;
-    int enqueueTime;
-    int dequeueTime;
-    int eqBlockNum;
-    int dqBlockNum;
-    char **string;
-    sem_t mutex; 
+    int enqueueBlockCount;
+    int dequeueBlockCount;
+    char **string;
+    sem_t mutex;
     sem_t full;
-    sem_t empty; 
+    sem_t empty;
     sem_t stat_block_mutex;
 } Queue;
+#endif
 
+//Function definitions for the Queue methods
 Queue *CreateStringQueue(int size);
-
 void EnqueueString(Queue *q, char *string);
-
 char * DequeueString(Queue *q);
-
 void PrintQueueStats(Queue *q);
 
-#endif
+
+//Function definitions for the Threads
+void *reader(void *string);
+void *munch1(void *queues);
+void *munch2(void *queues);
+void *writer(void *queue_data);
